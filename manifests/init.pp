1ffce08fd2d1d4c8b38f5158d6d5f353a46f7ad1
James Young <james.young@bjss.com>
2016-06-14 01:45:48 +0100

    Make server_host controllable from hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -51,11 +51,15 @@ class openam(
   $datastore   = 'embedded',
   $environment = hiera('openam::environment'),
   $master_url  = hiera('openam::master_url')
+  $server_host = hiera('openam::server_host', undef)
+
 ) {
-  $server_host = $::sso_server_hostname ? {
-    undef   => $::fqdn,
-    nil     => $::fqdn,
-    default => $::sso_server_hostname,
+  if $server_host == undef {
+    $server_host = $::sso_server_hostname ? {
+      undef   => $::fqdn,
+      nil     => $::fqdn,
+      default => $::sso_server_hostname,
+    }
   }
 
   $server_url = "${openam::server_protocol}://${openam::server_host}:${openam::server_port}"
